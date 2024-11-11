ef67e14b5395d0dc878b6c86860f6fe5bf214928
bloomgt <bloomgt@dukes.jmu.edu>
2024-11-11 13:19:25 -0500

    Commit 11/11 .3

---
+++ main/BatteryLifespan.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+import pandas as pd
+import scipy
+
+#Import Files
+files = [""]
+fileselect = files[]
+path = "../../../data/" + fileselect
+data = np.genfromtxt(path, delimiter=',')
+
