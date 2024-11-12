c6fb89d1e06b5df6d33d3ca0de9e6375f64fbbb3
hamil2nc <hamil2nc@dukes.jmu.edu>
2024-11-12 13:21:10 -0500

    Create subdir and move csv to correct pos

--- main/Import MetaData.py
+++ main/Import MetaData.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
+import os
+import shutil
 
-path_to_dir = "data/"
-metadata_path = "metadata.csv"
+file_path = "data/metadata.csv"
 
-file_path = path_to_dir + metadata_path
 
 df = pd.read_csv(file_path, header=0)
 
@@ -12,6 +12,23 @@ battery_names = np.unique(df['battery_id'])
 
 dict = {}
 
+
+try:
+    os.mkdir ("data/batteries")
+except:
+    print("Directories already created.")
+
 for id in battery_names:
     data = df[df['battery_id'] == id]
-    dict[id] = data['filename'].to_numpy()
+    csv_array = data['filename'].to_numpy()
+    dict[id] = csv_array
+    try:
+        os.mkdir("data/batteries/" + id)
+    except:
+        print("Sub-directory already created.")
+
+    file_dir = "data/data/"
+    move_to = "data/batteries/" + id
+    for file in csv_array:
+        shutil.move(file_dir + file, move_to)
+
