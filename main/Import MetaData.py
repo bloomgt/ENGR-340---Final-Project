8baf1f1585da806f3296a2af9f3d779946258b13
bloomgt <bloomgt@dukes.jmu.edu>
2024-11-12 11:50:00 -0500

    --

--- main/Import MetaData.py
+++ main/Import MetaData.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 import kagglehub
 
 # Download latest version
 path = kagglehub.dataset_download("patrickfleith/nasa-battery-dataset")
+data = np.genfromtxt(path, delimiter=',')
+
 
