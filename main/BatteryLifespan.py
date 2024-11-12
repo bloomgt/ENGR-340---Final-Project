4f70989b0be1c2b67eed6940d0de562d8118bf77
bloomgt <bloomgt@dukes.jmu.edu>
2024-11-12 11:42:28 -0500

    add file Import Metadata

--- main/BatteryLifespan.py
+++ main/BatteryLifespan.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 import scipy
 
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("patrickfleith/nasa-battery-dataset")
+
 #File Selection
 files = [""]
 fileselect = files[]
