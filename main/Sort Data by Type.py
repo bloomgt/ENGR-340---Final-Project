68e41984c717c2be4073f61fe0f118dadbf0ab97
hamil2nc <hamil2nc@dukes.jmu.edu>
2024-11-19 18:40:43 -0500

    Add comments to Import MetaData and Sort Data by Type

--- main/Sort Data by Type.py
+++ main/Sort Data by Type.py
@@ -3,15 +3,16 @@ import numpy as np
 import os
 import shutil
 
-# November 13, 2024
-# Bloom, Hamilton
-
+# Create list of all unique Battery ID's
 root_path = "data/batteries/"
 metadata_path = "data/metadata.csv"
 df = pd.read_csv(metadata_path, header=0)
 battery_names = np.unique(df['battery_id'])
 
+# Look at each unique battery
 for battery in battery_names:
+
+    # Create directories for the impedance, charging, and discharging data
     try:
         os.mkdir(root_path + battery + "/Impedance/")
         os.mkdir(root_path + battery + "/Charge/")
@@ -19,16 +20,20 @@ for battery in battery_names:
     except:
         print("Sub-directory already created")
 
+    # Create sub-dataframe for the current unique battery
     data = df[df['battery_id'] == battery]
 
+    #impedence, charge, discharge sub-dataframe
     idf = data[data['type'] == 'impedance']
     cdf = data[data['type'] == 'charge']
     ddf = data[data['type'] == 'discharge']
 
+    # create list of corresponding csv files
     icsv = idf['filename'].to_numpy()
     ccsv = cdf['filename'].to_numpy()
     dcsv = ddf['filename'].to_numpy()
 
+    # Move csv files to correct sub-directory
     try:
         for csv in icsv:
             shutil.move(root_path + battery + "/" + csv, root_path + battery + "/Impedance/")
@@ -39,5 +44,4 @@ for battery in battery_names:
     except:
         print("Files already moved.")
 
-    
-    
+# Repeat for next battery
